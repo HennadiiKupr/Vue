76d29864a286016dd7e0c95e12b1d845f84ba2dc
hennadii <hennadii.kupriiuk@gmail.com>
2023-09-14 14:45:22 +0300

    learned how props worked

---
+++ components/src/components/User.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>The user is {{ age }} years old</p>
+    <button type="button" @click="age++">Update Age</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'User',
+    props: ["age"]
+  }
+</script>
