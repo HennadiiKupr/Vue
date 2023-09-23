3a4230afdae01528aec7f43d2fa54fd2fb36ef8f
hennadii <hennadii.kupriiuk@gmail.com>
2023-09-23 14:22:01 +0300

    first work with dynamic components

---
+++ dynamic-components/src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <p>Home Page</p>
+</template>
+
+<script>
+  export default {
+    unmounted() {
+      console.log("home component unmounted")
+    },
+    activated() {
+      console.log("home component activated")
+    },
+    deactivated() {
+      console.log("home component deactivated")
+    }
+  }
+</script>
